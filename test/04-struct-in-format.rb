39225d51dfdee43257020004b4a66bd3759427f9
Miroslav Tynovsky <tynovsky@avast.com>
2016-02-22 13:11:52 +0100

    store hocon if requested (don't fallback to json)

--- test/04-struct-in-format.rb
+++ test/04-struct-in-format.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'java-properties'
 
 class StructInFormatTest < Minitest::Test
-    def test_struct_in_format
+    def test_struct_in_json
         storer = StructureButcher::Storer.new
         body = {
             "one" => [ "two", "three" ]
@@ -13,6 +13,17 @@ class StructInFormatTest < Minitest::Test
         expected_json = '{"one":["two","three"]}'
 
         assert_equal(generated_json, expected_json)
+    end
+
+    def test_struct_in_hocon
+        storer = StructureButcher::Storer.new
+        body = {
+            "one" => [ "two", "three" ]
+        }
+        generated_hocon = storer.structure_in_format(body, "hocon")
+        expected_hocon = "one=[\n    two,\n    three\n]\n"
+
+        assert_equal(generated_hocon, expected_hocon)
     end
 end
 
